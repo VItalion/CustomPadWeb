67facd9562bafb8b4dae003a426dd1136d68d40f
Vitalii <witalxp@gmail.com>
2026-01-19 20:26:41 +0200

    Fixed slider on Landing page

--- CustomPadWeb.Frontend/CustomPadWeb.Frontend/wwwroot/scripts/slideshow.js
+++ CustomPadWeb.Frontend/CustomPadWeb.Frontend/wwwroot/scripts/slideshow.js
@@ -1,28 +1,28 @@
-﻿let slideIndex = 1;
-showSlides(slideIndex);
+﻿let landingSlideIndex = 1;
+showSlides(landingSlideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(landingSlideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(landingSlideIndex = n);
 }
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { landingSlideIndex = 1 }
+    if (n < 1) { landingSlideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    //slides[slideIndex - 1].style.display = "block";
-    //dots[slideIndex - 1].className += " active";
+    slides[landingSlideIndex - 1].style.display = "block";
+    dots[landingSlideIndex - 1].className += " active";
 }
